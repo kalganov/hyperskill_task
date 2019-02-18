9d3557df620f48692a95667927b6cae532180110
Roman_Kalganov <roman_kalganov@epam.com>
2019-02-18 18:06:00 +0300

    first stage

---
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.example.myapplication", appContext.getPackageName());
+    }
+
+    @Test
+    public void textViewExist() {
+        onView(withId(R.id.textView)).perform(ViewActions.click());
+    }
+
+    @Test
+    public void startButtonExist() {
+        onView(withText("Start")).perform(ViewActions.click());
+    }
+
+    @Test
+    public void resetButtonExist() {
+        onView(withText("Reset")).perform(ViewActions.click());
+    }
+
+}
